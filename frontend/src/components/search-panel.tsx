0120132a2b44f573c98dac10b3ee87f4fa9cb5cc
Atif Galamov <kalemli@gmail.com>
2022-08-26 16:42:43 +0400

    trivial changes

--- frontend/src/components/search-panel.tsx
+++ frontend/src/components/search-panel.tsx
@@ -1,13 +1,9 @@
 import React, { useContext, useState } from 'react';
-import { debounce } from 'lodash';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
 import { MovieContext } from '../context';
 
 const useStyles = makeStyles((theme: Theme) =>
